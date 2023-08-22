eb8996744c1143380858cc4480b0adc95b9b055d
Niket16 <niketpatel1604@gmail.com>
2023-08-22 16:39:28 +0930

    Implemented 'Play Again' Feature in the Game + Test Cases Enhanced the game by adding a 'Play Again' option, allowing users to restart the excitement. Created comprehensive test cases to validate valid and invalid inputs for the 'Play Again' functionality.

--- guess_the_number.py
+++ guess_the_number.py
@@ -3,7 +3,7 @@ import random
 class GuessTheNumberGame:
     def __init__(self):
         self.number = str(random.randint(1000, 9999))
-        print(self.number)
+        # print(self.number)
         self.attempts = 0
 
     def play_game(self):
@@ -18,7 +18,25 @@ class GuessTheNumberGame:
             if guess == self.number:
                 print("Congratulations! You guessed the number correctly.")
                 print("Number of attempts:", self.attempts)
+                self.ask_to_play_again()
                 break
+
+    def ask_to_play_again(self):
+        self.attempts = 0
+        while True:
+            play_again = input("Do you want to play again? (y/n): ")
+            if play_again.lower() == 'y':
+                self.number = str(random.randint(1000, 9999))
+                # print(self.number)
+                self.attempts = 0
+                print("Let's play another exciting round! 🎮")
+                self.play_game()
+                break
+            elif play_again.lower() == 'n':
+                print("Thank you for playing!")
+                break
+            else:
+                print("Invalid input. Please enter 'y' or 'n'.")
 
 
 if __name__ == '__main__':
--- test_guess_the_number.py
+++ test_guess_the_number.py
@@ -8,6 +8,16 @@ class TestGuessTheNumberGame(unittest.TestCase):
     def setUp(self):
         self.game = GuessTheNumberGame()
 
+    @patch('builtins.input', side_effect=['1234','n'])
+    def test_correct_guess(self, mock_input):
+        with patch('sys.stdout', new_callable=StringIO) as mock_output:
+            self.game.number = '1234'
+            self.game.play_game()
+            output = mock_output.getvalue().strip()
+            self.assertIn("Congratulations! You guessed the number correctly.", output)
+            self.assertIn("Number of attempts: 1", output)
+            self.assertIn("Thank you for playing!", output)
+
     @patch('builtins.input', side_effect=['123a', '12345', 'abcd', '6789', '4567', '9876', 'q', '1234', 'q'])
     def test_invalid_inputs(self, mock_input):
         with patch('sys.stdout', new_callable=StringIO) as mock_output:
@@ -15,16 +25,32 @@ class TestGuessTheNumberGame(unittest.TestCase):
             output = mock_output.getvalue().strip()
             self.assertIn("Invalid input. Please enter a valid 4-digit number.", output)
             self.assertIn("Invalid input. Please enter a valid 4-digit number.", output)
-    
+            self.assertIn("Invalid input. Please enter a valid 4-digit number.", output)
 
-    @patch('builtins.input', side_effect=['1234'])
-    def test_correct_guess(self, mock_input):
+    @patch('builtins.input', side_effect=['y','q'])
+    def test_play_again_yes(self, mock_input):
         with patch('sys.stdout', new_callable=StringIO) as mock_output:
-            self.game.number = '1234'
-            self.game.play_game()
+            self.game.ask_to_play_again()
             output = mock_output.getvalue().strip()
-            self.assertIn("Congratulations! You guessed the number correctly.", output)
-
+            self.assertIn("Let's play another exciting round! 🎮", output)
+            self.assertEqual(self.game.attempts, 0) 
+        
+    @patch('builtins.input', side_effect=['n'])
+    def test_play_again_no(self, mock_input):
+        with patch('sys.stdout', new_callable=StringIO) as mock_output:
+            self.game.ask_to_play_again()
+            output = mock_output.getvalue().strip()
+            self.assertEqual(self.game.attempts, 0)
+            self.assertEqual("Thank you for playing!",output)  
+        
+    @patch('builtins.input', side_effect=['invalid', 'n'])
+    def test_play_again_invalid_then_yes_no(self, mock_input):
+        with patch('sys.stdout', new_callable=StringIO) as mock_output:
+            self.game.ask_to_play_again()
+            output = mock_output.getvalue().strip()
+            self.assertIn("Invalid input. Please enter 'y' or 'n'.", output)
+            self.assertIn("Thank you for playing!",output)
+            self.assertEqual(self.game.attempts, 0)  
 
 if __name__ == '__main__':
     unittest.main()

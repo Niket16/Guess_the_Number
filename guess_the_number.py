0dcd89d3d8ce537137d75c48855e8d636cc0bd13
Niket16 <niketpatel1604@gmail.com>
2023-08-24 13:27:51 +0930

    Implemented all suggested Pylint changes for improved code readability and adherence to standards.

--- guess_the_number.py
+++ guess_the_number.py
@@ -1,17 +1,37 @@
+"""
+    This module defines the GuessTheNumberGame class, which is a simple number guessing game.
+    Players attempt to guess a 4-digit number randomly chosen by the computer.
+
+    The game provides hints to guide the player towards the correct answer.
+
+    Usage:
+        Run this module directly to play the Guess The Number game.
+
+    Author:
+        Niket Patel
+
+    Date:
+        20/08/2023
+"""
+
 import random
 
 class GuessTheNumberGame:
+    """Number guessing game for 4-digit numbers."""
+
     def __init__(self):
         self.number = str(random.randint(1000, 9999))
         # print(self.number)
         self.attempts = 0
 
     def play_game(self):
+        """Play the number guessing game, providing hints and tracking attempts."""
+
         while True:
             guess = input("Enter your guess (4 digits) or 'q' to quit: ")
             if guess.lower() == 'q':
-             self.attempts = 0
-             break
+                self.attempts = 0
+                break
             if not guess.isdigit() or len(guess) != 4:
                 print("Invalid input. Please enter a valid 4-digit number.")
                 continue
@@ -22,40 +42,42 @@ class GuessTheNumberGame:
                 print("Number of attempts:", self.attempts)
                 self.ask_to_play_again()
                 break
-    
+
     def give_hints(self, guess):
+        """Generate and display hints for the guessed number."""
         hints = []
-        for i in range(len(guess)):
-            if guess[i] == self.number[i]:
+        for i, digit in enumerate(guess):
+            if digit == self.number[i]:
                 hints.append('O')
-            elif guess[i] in self.number:
+            elif digit in self.number:
                 hints.append('X')
             else:
                 hints.append('-')
         if len(hints) == 0:
             print("No digits are correct.")
         else:
-            if(" ".join(hints) != "O O O O"):
+            if " ".join(hints) != "O O O O":
                 print("Hints:", " ".join(hints))
 
     def ask_to_play_again(self):
+        """Reset game or exit based on player's choice."""
+
         self.attempts = 0
         while True:
             play_again = input("Do you want to play again? (y/n): ")
             if play_again.lower() == 'y':
                 self.number = str(random.randint(1000, 9999))
-                # print(self.number)
                 self.attempts = 0
+                # print(self.number)
                 print("Let's play another exciting round! 🎮")
                 self.play_game()
                 break
-            elif play_again.lower() == 'n':
+            if play_again.lower() == 'n':
                 print("Thank you for playing!")
                 break
-            else:
-                print("Invalid input. Please enter 'y' or 'n'.")
+            print("Invalid input. Please enter 'y' or 'n'.")
 
 
 if __name__ == '__main__':
-    game = GuessTheNumberGame()
-    game.play_game()
+    GAME = GuessTheNumberGame()
+    GAME.play_game()
--- test_guess_the_number.py
+++ test_guess_the_number.py
@@ -1,19 +1,45 @@
+"""
+This module contains unit tests for the GuessTheNumberGame class functionality.
+
+Tested functionality:
+- Generation of a random number within a valid range.
+- Correct guessing of the number with appropriate output messages.
+- Handling of various invalid inputs during gameplay.
+- Hints given for partially correct guesses.
+- No correct digits hints scenario.
+- Game replay scenarios (both play again and do not play again).
+- Comprehensive test scenario covering various gameplay scenarios.
+
+Author:
+    Niket Patel
+Date:
+    20/08/2023
+"""
+
 import unittest
 from unittest.mock import patch
 from io import StringIO
+import re
 from guess_the_number import GuessTheNumberGame
 
 class TestGuessTheNumberGame(unittest.TestCase):
+    """Unit tests for GuessTheNumberGame class functionality and scenarios."""
 
     def setUp(self):
+        """Set up common test environment before each test case."""
+
         self.game = GuessTheNumberGame()
         self.game.number = '1234'  # Set the default number
 
     def test_generate_random_number(self):
+        """Test random number generation within the valid range."""
+
         self.assertTrue(1000 <= int(self.game.number) <= 9999)
 
-    @patch('builtins.input', side_effect=['1234','n'])
-    def test_correct_guess(self, mock_input):
+    @patch('builtins.input', side_effect=['1234', 'n'])
+    def test_correct_guess(self, _):
+        """Test correct guessing, output messages, and attempts tracking."""
+
         with patch('sys.stdout', new_callable=StringIO) as mock_output:
             self.game.number = '1234'
             self.game.play_game()
@@ -22,17 +48,21 @@ class TestGuessTheNumberGame(unittest.TestCase):
             self.assertIn("Number of attempts: 1", output)
             self.assertIn("Thank you for playing!", output)
 
-    @patch('builtins.input', side_effect=['123a', '12345', 'abcd', '6789', '4567', '9876', 'q', '1234', 'q'])
-    def test_invalid_inputs(self, mock_input):
+    @patch('builtins.input', side_effect=['123a', '12345', 'abcd', '6789', '4567', '9876', 'q'])
+    def test_invalid_inputs(self, _):
+        """Test handling of various invalid inputs during gameplay."""
+
         with patch('sys.stdout', new_callable=StringIO) as mock_output:
             self.game.play_game()
             output = mock_output.getvalue().strip()
             self.assertIn("Invalid input. Please enter a valid 4-digit number.", output)
             self.assertIn("Invalid input. Please enter a valid 4-digit number.", output)
             self.assertIn("Invalid input. Please enter a valid 4-digit number.", output)
-    
+
     @patch('builtins.input', side_effect=["1253"])
-    def test_partial_correct_guess(self, mock_input):
+    def test_partial_correct_guess(self, _):
+        """Test hints for partially correct guesses and hints generation."""
+
         with patch('sys.stdout', new_callable=StringIO) as mock_output:
             self.game.number = "1234"
             self.game.give_hints(input())
@@ -42,7 +72,9 @@ class TestGuessTheNumberGame(unittest.TestCase):
             self.assertEqual(output, expected_output)
 
     @patch('builtins.input', side_effect=["5678"])
-    def test_no_correct_digits(self, mock_input):
+    def test_no_correct_digits(self, _):
+        """Test scenario with no correct digits in the hints."""
+
         with patch('sys.stdout', new_callable=StringIO) as mock_output:
             self.game.number = "1234"
             self.game.give_hints(input())
@@ -51,32 +83,61 @@ class TestGuessTheNumberGame(unittest.TestCase):
             expected_output = f"Hints: {expected_hints}"
             self.assertEqual(output, expected_output)
 
+    @patch('builtins.input', side_effect=['y', 'q'])
+    def test_play_again_yes(self, _):
+        """Test replay scenario where player chooses to play again."""
 
-    @patch('builtins.input', side_effect=['y','q'])
-    def test_play_again_yes(self, mock_input):
         with patch('sys.stdout', new_callable=StringIO) as mock_output:
             self.game.ask_to_play_again()
             output = mock_output.getvalue().strip()
             self.assertIn("Let's play another exciting round! 🎮", output)
-            self.assertEqual(self.game.attempts, 0) 
-        
+            self.assertEqual(self.game.attempts, 0)
+
     @patch('builtins.input', side_effect=['n'])
-    def test_play_again_no(self, mock_input):
+    def test_play_again_no(self, _):
+        """Test replay scenario where player chooses not to play again."""
+
         with patch('sys.stdout', new_callable=StringIO) as mock_output:
             self.game.ask_to_play_again()
             output = mock_output.getvalue().strip()
             self.assertEqual(self.game.attempts, 0)
-            self.assertEqual("Thank you for playing!",output)  
-        
+            self.assertEqual("Thank you for playing!", output)
+
     @patch('builtins.input', side_effect=['invalid', 'n'])
-    def test_play_again_invalid_then_yes_no(self, mock_input):
+    def test_play_again_invalid_then_yes_no(self, _):
+        """Test replay scenarios: invalid input, play again, and not play again."""
+
         with patch('sys.stdout', new_callable=StringIO) as mock_output:
             self.game.ask_to_play_again()
             output = mock_output.getvalue().strip()
             self.assertIn("Invalid input. Please enter 'y' or 'n'.", output)
-            self.assertIn("Thank you for playing!",output)
-            self.assertEqual(self.game.attempts, 0)  
+            self.assertIn("Thank you for playing!", output)
+            self.assertEqual(self.game.attempts, 0)
+
+    @patch('builtins.input', side_effect=[
+        '1234',  # Correct guess
+        'y',     # Play again
+        '0000',  # Incorrect guess
+        'abcd',  # Invalid input
+        '123a',  # Invalid input
+        'q'      # Quit
+    ])
+    def test_comprehensive_scenario(self, _):
+        """Test comprehensive gameplay scenarios and output messages."""
+
+        with patch('sys.stdout', new_callable=StringIO) as mock_output:
+            self.game.play_game()
+            output = mock_output.getvalue().strip()
+
+            # Assertions for different scenarios
+            self.assertIn("Congratulations! You guessed the number correctly.", output)
+            self.assertIn("Number of attempts: 1", output)
+            self.assertIn("Let's play another exciting round! 🎮", output)
+            self.assertRegex(output.split("\n")[3], re.compile("Hints: |-|O|X{4}"))
+            self.assertIn("Invalid input. Please enter a valid 4-digit number.", output)
+            self.assertIn("Invalid input. Please enter a valid 4-digit number.", output)
+            # Check that the attempts counter was reset after playing again
+            self.assertEqual(self.game.attempts, 0)
 
 if __name__ == '__main__':
     unittest.main()
-
